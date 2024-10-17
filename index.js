6c90178b1972c3b0b417d6c931ac10a9db047ce4
bj9322 <bj932218@gmail.com>
2024-10-17 10:07:21 +0900

    1일차 최종

--- index.js
+++ index.js
@@ -10,6 +10,27 @@ function solution(num1, num2) {
     return num1 * num2;
 }
 
+// 3. 두 수의 몫
+
 function solution(num1, num2) {
     return parseInt(num1 / num2);
+}
+
+// 4. 나이 구하기
+
+function solution(age) {
+    return (2023 - age);
+}
+
+// 5. 숫자 비교하기
+
+function solution(num1, num2) {
+    let result = (num1 === num2) ? 1 : -1;
+    return result;
+}
+
+// 6. 두 수의 합
+
+function solution(num1, num2) {
+    return num1 + num2;
 }
