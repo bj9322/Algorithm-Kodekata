1be7b2ac352948696e7dc76fa3eda401dc37d3b4
bj9322 <bj932218@gmail.com>
2024-10-18 10:30:49 +0900

    2일차 최종

--- index.js
+++ index.js
@@ -1,36 +1,72 @@
-// 1. 두 수의 차
+// // 1. 두 수의 차
 
-function solution(num1, num2) {
-    return num1 - num2;
-}
+// function solution(num1, num2) {
+//     return num1 - num2;
+// }
 
-// 2. 두 수의 곱
+// // 2. 두 수의 곱
 
-function solution(num1, num2) {
-    return num1 * num2;
-}
+// function solution(num1, num2) {
+//     return num1 * num2;
+// }
 
-// 3. 두 수의 몫
+// // 3. 두 수의 몫
 
-function solution(num1, num2) {
-    return parseInt(num1 / num2);
-}
+// function solution(num1, num2) {
+//     return parseInt(num1 / num2);
+// }
 
-// 4. 나이 구하기
+// // 4. 나이 구하기
 
-function solution(age) {
-    return (2023 - age);
-}
+// function solution(age) {
+//     return (2023 - age);
+// }
 
-// 5. 숫자 비교하기
+// // 5. 숫자 비교하기
 
-function solution(num1, num2) {
-    let result = (num1 === num2) ? 1 : -1;
-    return result;
-}
+// function solution(num1, num2) {
+//     let result = (num1 === num2) ? 1 : -1;
+//     return result;
+// }
 
-// 6. 두 수의 합
+// // 6. 두 수의 합
 
-function solution(num1, num2) {
-    return num1 + num2;
-}
+// function solution(num1, num2) {
+//     return num1 + num2;
+// }
+
+// // 7. 두 수의 나눗셈
+
+// function solution(num1, num2) {
+//     return parseInt((num1 / num2) * 1000)
+// }
+
+// 8. 각도기
+
+// const angle = 100;
+// function solution(angle) {
+//     if(angle > 0 && angle < 90){
+//         return 1;
+//     }
+//     else if(angle === 90){
+//         return 2;
+//     }
+//     else if(angle > 90 && angle < 180){
+//         return 3;
+//     }
+//     else if(angle === 180){
+//         return 4;
+//     }
+// }
+
+// 9. 짝수의 합
+
+// function solution(n) {
+//     let result = 0; 
+//     for(let i = 1; i <= n; i++) {
+//         if(i % 2 === 0) { 
+//             result += i; 
+//         }
+//     }
+//     return result; 
+// }
